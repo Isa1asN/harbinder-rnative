647e051ca59ab20672f4a82f6f796abcfca83676
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-10-16 16:00:37 -0700

    changes

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello there</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen 
+        name='home'
+        options={{headerTitle : "Home page"}}
+        component={Home}
+      />
+    </Stack.Navigator>
   );
 }
 
