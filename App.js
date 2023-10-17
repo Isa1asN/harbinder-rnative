3bf952531713236392d8e8cc5dbd9ba969763bde
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-10-16 18:24:03 -0700

    made new changes

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/Home';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>App page</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <Stack.Navigator initialRouteName='home' screenOptions={{headerShown:false}}>
+        <Stack.Screen name='home' component={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
