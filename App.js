edfe6b9ed3a543a13d28d4dc50f83ea6cfe993e9
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-10-17 13:39:29 -0700

    change

--- App.js
+++ App.js
@@ -11,7 +11,21 @@ export default function App() {
   return (
     <NavigationContainer >
       <Stack.Navigator initialRouteName='home' >
-        <Stack.Screen name='home' component={Home}/>
+        <Stack.Screen
+         name='home' 
+         component={Home} 
+         options={{
+          title : 'Home page',
+          headerStyle :{
+            backgroundColor : 'orange',
+            borderBottomEndRadius : 30
+            // borderTopRightRadius:110
+          },
+          headerTitleStyle : {
+            color : 'red',
+          },
+          headerTitleAlign : 'center'
+          }} />
         <Stack.Screen name='note' component={Note}/>
       </Stack.Navigator>
     </NavigationContainer>
