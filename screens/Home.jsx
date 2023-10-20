3c10d1e641732d867d91c52f6e42a493860d6fec
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-10-19 22:49:30 -0700

    changes

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -21,10 +21,17 @@ const Home = () => {
     <>
     <StatusBar animated={true}  backgroundColor='black'/>
     <View >
+      <View style={{backgroundColor:'rgba(0, 0, 0, .4)', width:'100%', height:'100%', position:'absolute'}}>
+      </View>
       <Image 
         source={note}
-        style={{width:'100',  }}
+        style={{width:'100', opacity:0.4}}
       />
+        <Text >
+        Welcome to Memopad!
+        </Text>
+      <View>
+      </View>
     </View>
     </>
   )
