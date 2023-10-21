1d101c1b6b145cda53bfbb5f0e40afe5b48b7334
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-10-21 10:22:00 -0700

    project major changes ...

--- App.js
+++ App.js
@@ -2,20 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Home from './screens/Home';
-import Note from './screens/Note';
+import Welcome from './screens/Welcome';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer >
-      <Stack.Navigator initialRouteName='home' >
+      <Stack.Navigator initialRouteName='welcome' >
         <Stack.Screen
-         name='home' 
-         component={Home} 
+         name='welcome' 
+         component={Welcome} 
          options={{headerShown : false}} />
-        <Stack.Screen name='note' component={Note}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
